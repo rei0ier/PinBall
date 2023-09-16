70cbdc3459bf2f0731bb8464c0005503e8fc12e3
rei0ier <139188412+rei0ier@users.noreply.github.com>
2023-09-16 13:44:08 +0900

    スコア表示、加算

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -11,18 +11,50 @@ public class BallController : MonoBehaviour
     //ゲームオーバーを表示するテキスト
     private GameObject gameoverText;
 
-   
+    //Score
+    private int score = 0;
+
+    //Score表示テキスト
+    private GameObject ScoreText;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "LargeStar")
+        {
+            score += 20;
+        }
+        if (collision.gameObject.tag == "SmallStar")
+        {
+            score += 30;
+        }
+        if (collision.gameObject.tag == "LargeCloud")
+        {
+            score += 50;
+        }
+        if (collision.gameObject.tag == "SmallCloud")
+        {
+            score += 40;
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
         //シーン中のGameOverTextオブジェクトを取得
         this.gameoverText = GameObject.Find("GameOverText");
+
+        //シーン中のScoreTextオブジェクトを取得
+        this.ScoreText = GameObject.Find("ScoreText");
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Scoreを表示
+        this.ScoreText.GetComponent<Text>().text = "Score:" + score;
+
         //ボールが画面外に出た場合
         if (this.transform.position.z < this.visiblePosZ)
         {
